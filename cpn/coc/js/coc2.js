b0fb9983f381c44bbedf5446dfe58719440fac07
mglepw <mglepw@gmail.com>
2016-05-14 18:29:12 -0300

    Modelo COC2

---
+++ cpn/coc/js/coc2.js
@@ -0,0 +1,20 @@
+// cpn_coc
+var coc = document.getElementById("coc");
+coc.setAttribute("id", "coc");
+var coc_img1 = document.createElement("img");
+var coc_img2 = document.createElement("img");
+var coc_img3 = document.createElement("img");
+var coc_img4 = document.createElement("img");
+var coc_img5 = document.createElement("img");
+
+var object_coc = new Mgl_acpn();
+object_coc.setEffect("fade, scrollRight");
+object_coc.setLength(226, 226);
+object_coc.setNodeUp(coc);
+object_coc.append_elem(coc_img1, "http://mglepw.github.io/cpn/coc/img/t1.png");
+object_coc.append_elem(coc_img2, "http://mglepw.github.io/cpn/coc/img/t2.png");
+object_coc.append_elem(coc_img3, "http://mglepw.github.io/cpn/coc/img/t3.png");
+object_coc.append_elem(coc_img4, "http://mglepw.github.io/cpn/coc/img/t4.png");
+object_coc.append_elem(coc_img5, "http://mglepw.github.io/cpn/coc/img/t5.png");
+object_coc.createGUI();
+object_coc.play();
