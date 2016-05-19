be54e94b303cd5c50c243eab66a89121a58502be
mglepw <mglepw@gmail.com>
2016-05-19 14:01:40 -0300

    inclusão boticario

---
+++ cpn/btcr/js/btcr.js
@@ -0,0 +1,16 @@
+var btcr = document.getElementById("coc");
+var btcr_img1 = document.createElement("img");
+var btcr_img2 = document.createElement("img");
+var btcr_img3 = document.createElement("img");
+var btcr_img4 = document.createElement("img");
+
+var object_btrc = new Mgl_acpn();
+object_btrc.setEffect("fade");
+object_btrc.setLength(226, 226);
+object_btrc.setNodeUp(btcr);
+object_btrc.append_elem(btcr_img1, "http://mglepw.github.io/cpn/btcr/img/img1.png");
+object_btrc.append_elem(btcr_img2, "http://mglepw.github.io/cpn/btcr/img/img2.png");
+object_btrc.append_elem(btcr_img3, "http://mglepw.github.io/cpn/btcr/img/img3.png");
+object_btrc.append_elem(btcr_img4, "http://mglepw.github.io/cpn/btcr/img/img4.png");
+object_btrc.createGUI();
+object_btrc.play();
