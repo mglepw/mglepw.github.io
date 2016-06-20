2ee49d35f3be48851fa9655c7faacb97981ec7ad
mglepw <mglepw@gmail.com>
2016-06-20 20:51:52 -0300

    radio

---
+++ cpn/radio/js/radio.js
@@ -0,0 +1,10 @@
+// radio
+var radio = document.getElementById("radio");
+var object_radio = new Mgl_acpn2();
+object_radio.setEffect("fade");
+object_radio.setLength(226, 226);
+object_radio.setNodeUp(radio);
+object_radio.append_elem("http://mglepw.github.io/cpn/radio/img/bdl.png");
+object_radio.append_elem("http://mglepw.github.io/cpn/radio/img/ccm.png");
+object_radio.createGUI();
+object_radio.play();
---
+++ js/mglepw2.js
@@ -0,0 +1,35 @@
+var Mgl_acpn2 = function() {
+	this.list_elem = [];
+	this.nodeUp = null;
+	this.effect = "scrollDown";
+	this.width = 226;
+	this.height = 226;
+
+	this.append_elem = function(img) {
+		var elem = document.createElement("img");
+		elem.setAttribute("src", img);
+		elem.setAttribute("width", this.width);
+		elem.setAttribute("height", this.height);
+		this.list_elem.push(elem);
+	}
+	this.setLength = function(wght, hght) {
+		this.weight = wght;
+		this.height = hght;
+	}
+	this.setNodeUp = function(nUp) {
+		this.nodeUp = nUp;
+	}
+	this.createGUI = function() {
+		for (var i = 0; i < this.list_elem.length; i++) {
+			this.nodeUp.appendChild(this.list_elem[i]);
+		}
+	}
+	this.setEffect = function(effct) {
+		this.effect = effct;
+	}
+	this.play = function() {
+		$(this.nodeUp).cycle({
+			fx: this.effect
+		});
+	}
+}
