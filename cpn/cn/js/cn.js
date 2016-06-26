7bf529a70bd6d62e42c52452e9b613001e831466
mglepw <mglepw@gmail.com>
2016-06-26 13:34:17 -0300

    Casa Nova

---
+++ cpn/cn/js/cn.js
@@ -0,0 +1,13 @@
+// cn
+var cn = document.getElementById("cn");
+var object_cn = new Mgl_acpn2();
+object_cn.setEffect("fade");
+object_cn.setLength(226, 226);
+object_cn.setNodeUp(cn);
+object_cn.append_elem("https://3.bp.blogspot.com/-QRY3xCxRJHY/V2_-EkWENvI/AAAAAAAADrY/3lIv0kMkjVYbvS5xUEk1UK2ZG0TjCnQEACLcB/s1600/cn1.png");
+object_cn.append_elem("https://3.bp.blogspot.com/-re454YJilFM/V2_-EjRz27I/AAAAAAAADrU/qTeQjFIaAfAzJkhtH8XFBXn9yvENHPt2QCLcB/s1600/cn2.png");
+object_cn.append_elem("https://4.bp.blogspot.com/-2CjHvChV5yc/V2_-E2S_55I/AAAAAAAADrc/2ith-vrxkG8aw04Fshrj7atE7LXztuOtQCLcB/s1600/cn4.png");
+object_cn.append_elem("https://1.bp.blogspot.com/-5mA5cQhDKGs/V2_-RnKAbEI/AAAAAAAADro/-zxMLviHANsCEQCs4BC7EDhiqUNGT3w8gCLcB/s1600/cn3.png");
+object_cn.createGUI();
+object_cn.setTimeout(1000);
+object_cn.play();
