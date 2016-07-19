3cd3d5792c94c5fe318b783accf7e49be5f1d4d9
mglepw <mglepw@gmail.com>
2016-07-19 19:49:04 -0300

    Nagel Contabilidade

---
+++ cpn/ngl/js/ngl.js
@@ -0,0 +1,12 @@
+// ngl
+var ngl = document.getElementById("ngl");
+var object_ngl = new Mgl_acpn2();
+object_ngl.setEffect("fade");
+object_ngl.setLength(226, 226);
+object_ngl.setNodeUp(ngl);
+object_ngl.append_elem("http://mglepw.github.io/cpn/ngl/img/ngl1.png");
+object_ngl.append_elem("http://mglepw.github.io/cpn/ngl/img/ngl2.png");
+object_ngl.append_elem("http://mglepw.github.io/cpn/ngl/img/ngl3.png");
+object_ngl.createGUI();
+object_ngl.setTimeout(9400);
+object_ngl.play();
