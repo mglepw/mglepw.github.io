661beb967cca3098959b3e08909d51b733759fdc
iomltn <iomltn@gmail.com>
2016-10-20 12:47:40 -0300

    sonho lindo

---
+++ cpn/sl/js/sl.js
@@ -0,0 +1,11 @@
+// Sonho Lindo - 20/10/2016
+var sl = document.getElementById("sl");
+var object_sl = new Mgl_acpn2();
+object_sl.setEffect("fade");
+object_sl.setLength(226, 226);
+object_sl.setNodeUp(sl);
+object_sl.append_elem("http://mglepw.github.io/cpn/sl/img/sl1.png");
+object_sl.append_elem("http://mglepw.github.io/cpn/sl/img/sl2.png");
+object_sl.createGUI();
+object_sl.setTimeout(6400);
+object_sl.play();
