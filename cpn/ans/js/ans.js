c042c65f3439aeab1434c4e9dfc9e9c7047e1eaa
Milton Rodrigues <iomltn@gmail.com>
2017-08-31 13:03:35 -0300

    advogado nathaniel silveira

---
+++ cpn/ans/js/ans.js
@@ -0,0 +1,14 @@
+// ans
+var ans = document.getElementById("ans");
+var object_ans = new Mgl_acpn2();
+object_ans.setEffect("fade");
+object_ans.setLength(226, 226);
+object_ans.setNodeUp(ans);
+object_ans.append_elem("http://mglepw.github.io/cpn/ans/img/ans1.png");
+object_ans.append_elem("http://mglepw.github.io/cpn/ans/img/ans2.png");
+object_ans.append_elem("http://mglepw.github.io/cpn/ans/img/ans3.png");
+object_ans.append_elem("http://mglepw.github.io/cpn/ans/img/ans4.png");
+object_ans.append_elem("http://mglepw.github.io/cpn/ans/img/ans5.png");
+object_ans.createGUI();
+object_ans.setTimeout(9400);
+object_ans.play();
