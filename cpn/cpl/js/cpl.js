f6a235175cb3a9ad46efff501da95402b4a1b4e5
Milton Rodrigues <iomltn@gmail.com>
2017-09-09 12:13:55 -0300

    cellphone

---
+++ cpn/cpl/js/cpl.js
@@ -0,0 +1,13 @@
+// Variant- 07/09/2017
+var cpl = document.getElementById("cpl");
+var object_cpl = new Mgl_acpn2();
+object_cpl.setEffect("scrollRight");
+object_cpl.setLength(226, 226);
+object_cpl.setNodeUp(cpl);
+object_cpl.append_elem("http://mglepw.github.io/cpn/cpl/img/cpl1.png");
+object_cpl.append_elem("http://mglepw.github.io/cpn/cpl/img/cpl2.png");
+object_cpl.append_elem("http://mglepw.github.io/cpn/cpl/img/cpl3.png");
+object_cpl.append_elem("http://mglepw.github.io/cpn/cpl/img/cpl4.png");
+object_cpl.createGUI();
+object_cpl.setTimeout(6400);
+object_cpl.play();
